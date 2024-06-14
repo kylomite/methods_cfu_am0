c14b4ce2558612d3b17f5e8a6b69922fab31b30e
Kyle Delaney <kyle@Kyles-MBP.fios-router.home>
2024-06-14 12:25:07 -0400

    add answers 1&2 to built_in_methods.rb

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -7,22 +7,32 @@
 # No arguments are passed; downcase has one clear job which is to lowercase all letters that exist in the String
 # The return value is "hello world"
 "Hello World".downcase
-
+#The .downcase method requires no arguments when applied to a srting. the downcase method returned 
+#a new string, a copy of the original string object with every instance of an uppercase letter switched to 
+#its lowercase counterpart
 
 "Hello World".include?("Hello")
-
+# This method is ran on the string "Hello World". the method checks to see if the input string contains the 
+#specified argument, in this case it is asking to see if the string includes the sub-string "Hello".
+# it returns a boolean value based on if the string includes  the argument
 
 "Hello World".end_with?("Hello")
-
+#The .end_with? method is applied to the string "Hello World", it is given an argument of "Hello". It 
+#returns the boolean value false. This method is checking if the input string ends with a sub string 
+#specified as an argument of the method.
 
 "Hello World".end_with?("rld")
-
+#The .end_with? method is applied to the string "Hello World", it is given an argument of "rld". It 
+#returns the boolean value true. This method is checking if the input string ends with a sub string 
+#specified as an argument of the method.
 
 12.even?
-
+#The .even? method is applied to the interger 12. it returns the boolean value true. The method is checking
+#whether or not its being called on an even number, if its called on a odd number it will return false
 
 18.next
-
+#The .next method is being applied to an integer 18. it returns the integer 19. This method is acting as a 
+#counter for the integer we call it on.
 
 
 # SECTION 2: Calling methods on variables assigned to strings.
@@ -38,6 +48,21 @@
 # The puts command prints the return value of the start_with? method (true) to the console.
 first_name = "Jeff"
 puts first_name.start_with?("J")
+
+name1 = "Luffy"
+name2 = "Georgia"
+
+puts name1.upcase
+# The .upcase method is called on the name1 variable, which stores the string object "Luffy". 
+# The .upcase method returns "LUFFY", it will convert any string to all uppercase letters.
+# The puts command prints the return value of the .upcase method ("LUFFY") to the console.
+
+
+puts name2.index("r")
+# The .index method is called on the name2 variable, which stores the string object "Georgia". 
+# The .index method returns a zero-indexed string represening the first occurance of the speficed argument
+# In this example, the return value is 3 because the argment "r" occurs at the index location 3.
+# The puts command prints the return value of the .index() method (3) to the console.
 
 
 
